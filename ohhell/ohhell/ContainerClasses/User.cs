7068649a8e40ec0060ea3b9acc07c1e81e55a094
samcastelaz <sam.castelaz@gmail.com>
2018-07-22 17:13:37 -0400

    Add getters and setters

--- ohhell/ohhell/ContainerClasses/User.cs
+++ ohhell/ohhell/ContainerClasses/User.cs
@@ -10,5 +10,9 @@ namespace ohhell.ContainerClasses
         private string userName;
         private string password;
         private int totalScore;
+
+        public string UserName { get => userName; set => userName = value; }
+        public string Password { get => password; set => password = value; }
+        public int TotalScore { get => totalScore; set => totalScore = value; }
     }
 }
