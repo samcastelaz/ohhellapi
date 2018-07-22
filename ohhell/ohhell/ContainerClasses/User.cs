553827a55253782ca846b7dde1b76deefcf67b97
samcastelaz <sam.castelaz@gmail.com>
2018-07-22 17:10:28 -0400

    Add first class

---
+++ ohhell/ohhell/ContainerClasses/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ohhell.ContainerClasses
+{
+    public class User
+    {
+        private string userName;
+        private string password;
+        private int totalScore;
+    }
+}
