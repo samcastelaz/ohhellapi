f9201d2e743e3fc23dfe5815f619417650d1ba4e
samcastelaz <sam.castelaz@gmail.com>
2018-09-16 14:12:40 -0400

    Add game and nunit

---
+++ ohhell/ohhell/ContainerClasses/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ohhell.ContainerClasses
+{
+    public class Game
+    {
+
+        private String gameId;
+
+        public string GameId { get => gameId; set => gameId = value; }
+    }
+}
